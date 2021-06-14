5e067f6d6fb110d0998ec30723e4a0333771019b
Rita Varchenko <marharyta@y-tree.com>
2021-06-14 17:57:01 +0300

    Initial Commit

---
+++ iOSCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSCalculator
+//
+//  Created by Amac on 14.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
